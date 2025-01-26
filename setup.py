1978ac1b0ee9ff80237d54b1cc2dcd2cd554097a
vega <vega4792@gmail.com>
2025-01-26 15:43:04 +0900

    .

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcmaker',
-    version='0.0.1',
+    version='0.0.2',
     description='testcase maker for online judge',
     author='csvega',
     author_email='vega4792@gmail.com',
     url='https://github.com/csvega/tcmaker',
-    install_requires=['random', 'os', 're',],
+    install_requires=[],
     packages=find_packages(exclude=[]),
     keywords=['testcase', 'online judge', 'OJ', 'hustoj'],
     python_requires='>=3.6',
