4bffe69af5a88537ab926f91f7bd0e20e1f7c9d5
vega <vega4792@gmail.com>
2025-01-26 15:23:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='Simple Testcase Maker',
     version='0.0.1',
-    description='PYPI tutorial package creation written by csvega',
+    description='tcmaker package creation written by csvega',
     author='csvega',
     author_email='vega4792@gmail.com',
-    url='https://github.com/teddylee777/teddynote',
+    url='https://github.com/csvega/tcmaker',
     install_requires=['random', 'os', 're',],
     packages=find_packages(exclude=[]),
     keywords=['testcase', 'online judge', 'OJ', 'hustoj'],
